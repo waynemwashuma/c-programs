088cbcbe92faae640ef334c99b5037e8883c4167
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2023-10-19 08:22:41 -0700

    added feature to compund calculator to support float values in number of years

--- compoundInterest/main.c
+++ compoundInterest/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include <stddef.h>
+#include <math.h>
 
 void exitSmooth(int error)
 {
@@ -13,21 +13,26 @@ void exitSmooth(int error)
 
 int main()
 {
-    float principle, rate = 10.0, time, interest,totalInterest;
+    float principle, rate = 10.0, time, interest, totalInterest;
     printf("Enter the principle amount:");
     scanf("%f", &principle);
     printf("Enter the number of years:");
     scanf("%f", &time);
-    //interest = principle * rate / 100;
-    //amount = principle + interest;
+    // interest = principle * rate / 100;
+    // amount = principle + interest;
     for (float i = 1; i <= time; i++)
     {
         interest = principle * rate / 100;
         principle = principle + interest;
         totalInterest += interest;
-        printf("Interest of the %0.0f year: %0.2f\n",i,interest);
+        printf("Interest of the %0.0f year: %0.2f\n", i, interest);
     }
-    printf("\nTotal compound interest: %.2f\n",totalInterest);
+    float remainTime = time - floorf(time);
+    interest = remainTime * principle * rate / 100 ;
+    principle = principle + interest;
+    totalInterest += interest;
+    printf("Interest of the %0.0f year: %0.2f\n", ceilf(time), interest);
+    printf("\nTotal compond interest: %.2f",principle);
     exitSmooth(1);
     return 0;
 }
